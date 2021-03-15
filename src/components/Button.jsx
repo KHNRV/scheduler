f74eaa1f6c802b5b506e9a00ccd5f3e2221c06c7
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-15 19:39:34 -0400

    Create Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+  const { confirm, danger, disabled, onClick } = props;
+
+  const buttonClass = classNames(
+    "button",
+    { "button--confirm": confirm },
+    { "button--danger": danger }
+  );
+
+  return (
+    <button className={buttonClass} onClick={onClick} disabled={disabled}>
+      {props.children}
+    </button>
+  );
 }
