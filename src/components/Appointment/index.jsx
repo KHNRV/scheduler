d9c80ef8090b60b1f37b45b700c5b1b02f896ab8
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-16 20:02:01 -0400

    Implement Appointment with conditionnal state

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
 import "./styles.scss";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          interviewer={props.interview.interviewer}
+          student={props.interview.student}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
