724d31ce666bc20f09b7fdaa13eb9d0503f88b18
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-18 14:57:14 -0400

    Create new selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,3 +17,31 @@ export const getAppointmentsForDay = (state, day) => {
 
   return appointmentsObjects;
 };
+
+export const getInterview = (state, appointment) => {
+  if (!appointment || !appointment.interviewer) {
+    return null;
+  }
+
+  const interviewerId = appointment.interviewer;
+  const interview = {
+    student: appointment.student,
+    interviewer: state.interviewers[interviewerId],
+  };
+
+  return interview;
+};
+
+export const getInterviewersForDay = (state, day) => {
+  if (state.days.length === 0 || !state.days.find((val) => val.name === day)) {
+    return [];
+  }
+
+  const { interviewers } = state.days.find((value) => value.name === day);
+
+  const interviewersObjects = interviewers.map(
+    (id) => state.interviewers[id] || null
+  );
+
+  return interviewersObjects;
+};
