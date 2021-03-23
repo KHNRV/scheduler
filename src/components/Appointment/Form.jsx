26bb7c72ea499dae5d3f213fb6af1d30bd28490d
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-03-22 20:12:24 -0400

    Implement new tests

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   /**
    * Reset form
@@ -22,6 +23,16 @@ export default function Form(props) {
     reset();
   };
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -33,23 +44,25 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
             /*
           This must be a controlled component
         */
           />
+          <section className="appointment__validation">{error}</section>
+          <InterviewerList
+            interviewers={props.interviewers}
+            value={interviewer}
+            onChange={setInterviewer}
+          />
         </form>
-        <InterviewerList
-          interviewers={props.interviewers}
-          value={interviewer}
-          onChange={setInterviewer}
-        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>
+          <Button onClick={() => validate()} confirm>
             Save
           </Button>
         </section>
---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Appointment from "components/Appointment";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  render(<Appointment />);
+});
